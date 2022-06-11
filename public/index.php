d4b7f6a6473cb08e22dc591479f8bc9fb1bcba67
n4th4nrjesus <n4th4nrjesus@gmail.com>
2022-06-11 20:56:57 -0300

    bootstrap stylesheet configuration

--- public/index.php
+++ public/index.php
@@ -1,86 +1,55 @@
 <?php
-//echo 'teste';
-?>
 
-<!DOCTYPE html>
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-<head>
-    <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <title>Sprint Up</title>
-</head>
+define('LARAVEL_START', microtime(true));
 
-<body>
-    <style>
-        body {
-            background: #EBEBF0;
-        }
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-        .col-auto {
-            background: #657ED4;
-            padding-left: 25px;
-        }
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-        .card-title {
-            cursor: move;
-        }
-    </style>
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-    <nav class="navbar navbar-expand-lg" style="background-color: #657ED4;">
-        <div class="container-fluid">
-            <a class="navbar-brand text-white" href="index.php">Home</a>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="navbar-brand text-white">algo</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Digite algo" ">
-                    <button class=" btn btn-light" type="submit">Buscar</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+require __DIR__ . '/../vendor/autoload.php';
 
-    <div class="container">
-        <div class="row gx-5">
-            <div class="col-auto">
-                <h2 class=" fw-normal">Backlog</h1>
-                    <div class="card pe-auto" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">Teste</h5>
-                            <p class="card-text">Teste do teste
-                            <p class="card-text">
-                        </div>
-                    </div>
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">Teste</h5>
-                            <p class="card-text">Teste do teste
-                            <p class="card-text">
-                        </div>
-                    </div>
-            </div>
-            <div class="col-auto">
-                <h2 class="fw-normal">To do</h1>
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">Teste</h5>
-                            <p class="card-text">Teste do teste
-                            <p class="card-text">
-                        </div>
-                    </div>
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">Teste</h5>
-                            <p class="card-text">Teste do teste
-                            <p class="card-text">
-                        </div>
-                    </div>
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
-            </div>
-        </div>
-        <script>
-        </script>
-</body>
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
